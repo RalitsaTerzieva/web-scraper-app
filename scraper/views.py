0d4cf982baa77c62fdfa65d6e8950c1cf93c91f2
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-13 21:08:15 +0300

    Add view, template and url

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+
+def scrape(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    link_address = []
+    
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+        
+    return render(request, 'scrape/result.html', {'link_address': link_address})
