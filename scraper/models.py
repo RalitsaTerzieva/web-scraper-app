51c92de4cc5aa8bf18c36a9740bfb229e821d9ea
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-14 15:15:30 +0300

    Add Link model and register model to admin pannel

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    address = models.CharField(max_length=1000,null=True)
+    name = models.CharField(max_length=1000,null=True)
+    
+    def __str__(self):
+        return self.name
--- scraper/views.py
+++ scraper/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from .models import Link
 
 
 def scrape(request):
     page = requests.get('https://www.google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
-    link_address = []
+    
     
     for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
+        link_address = link.get('href')
+        link_text = link.text
+        Link.objects.create(address=link_address,name=link_text)
         
-    return render(request, 'scrape/result.html', {'link_address': link_address})
+    data = Link.objects.all()
+        
+    return render(request, 'scrape/result.html', {'data': data})
