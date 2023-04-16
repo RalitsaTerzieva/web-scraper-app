6a3f1def0bc45586decf075db9a11328c91ebe01
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-16 12:38:56 +0500

    Add delete and scrape logic

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from scraper import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',views.scrape,name='scrape'),
+    path('delete/',views.delete,name='delete'),
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 from .models import Link
+from django.http import HttpResponseRedirect
 
 
 def scrape(request):
-    page = requests.get('https://www.google.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    if request.method == 'POST':
+        link = request.POST.get('site','')
+        page = requests.get(link)
+        soup = BeautifulSoup(page.text, 'html.parser')
     
     
-    for link in soup.find_all('a'):
-        link_address = link.get('href')
-        link_text = link.text
-        Link.objects.create(address=link_address,name=link_text)
-        
-    data = Link.objects.all()
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.text
+            Link.objects.create(address=link_address,name=link_text)
+            return HttpResponseRedirect('/')
+    else:        
+        data = Link.objects.all()
         
     return render(request, 'scrape/result.html', {'data': data})
+
+def delete(request):
+    Link.objects.all().delete()
+    return render(request,'scrape/result.html')
