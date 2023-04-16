fe7e595cd0212400e462e34df7b269775ec6dcd6
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-16 10:59:00 +0500

    Add blank in Model

--- scraper/models.py
+++ scraper/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Link(models.Model):
-    address = models.CharField(max_length=1000,null=True)
-    name = models.CharField(max_length=1000,null=True)
+    address = models.CharField(max_length=1000,null=True,blank=True)
+    name = models.CharField(max_length=1000,null=True,blank=True)
     
     def __str__(self):
         return self.name
